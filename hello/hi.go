842b1a8c60a69dba9b9f10df2a149a6918321b0a
63isOK <1876180681@qq.com>
2019-01-23 09:30:55 +0000

    add write go

--- hello/hi.go
+++ hello/hi.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/63isOK/golearn/stringutil"
+)
 
 func main(){
 	fmt.Println("hello world.")
+	fmt.Println(stringutil.Reverse("hello world."))
 }
