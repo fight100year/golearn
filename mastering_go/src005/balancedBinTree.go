7341515ab7632b23b09fae0c0c37d1900f2acc51
63isOK <1876180681@qq.com>
2019-03-29 05:55:55 +0000

    完成第一个AVL高度平衡树 并添加一个实现丑陋的pretty print

--- mastering_go/src005/balancedBinTree.go
+++ mastering_go/src005/balancedBinTree.go
@@ -1,12 +1,17 @@
 // AVL二叉树,发明者: G. M. Adelson-Velsky和E. M. Landis
 // 高度平衡树,任何节点的两子树的高度最大差别为1
 // 递归版本
+// todo:
+// 1: 线可以优化一下，最底层的线和其他线的计算可以合并一下
+// 2: pretty print 单位从1增加到2 不使用float64
+// 3: 整个AVL代码的结构还可以整理一下，现在的版本有些随意
 
 package main
 
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -18,6 +23,7 @@ type AVL struct {
 	Right  *AVL
 	data   int
 	height int
+	pos    float64
 }
 
 func (avl *AVL) add(i int) *AVL {
@@ -56,8 +62,13 @@ func (avl *AVL) check() *AVL {
 	}
 
 	checkNode := avl.Parent.Parent
-	leftDepth := checkNode.Left.depth()
-	rightDepth := checkNode.Right.depth()
+	leftDepth, rightDepth := checkNode.height, checkNode.height
+	if checkNode.Left != nil {
+		leftDepth = checkNode.Left.depth()
+	}
+	if checkNode.Right != nil {
+		rightDepth = checkNode.Right.depth()
+	}
 	if dif := leftDepth - rightDepth; dif == 1 || dif == 0 || dif == -1 {
 		return avl.Parent.check()
 	}
@@ -148,9 +159,15 @@ func (avl *AVL) LL(checkNode *AVL) *AVL {
 	if leftSonRightSon != nil {
 		checkNode.Left = leftSonRightSon
 		leftSonRightSon.Parent = checkNode
+	} else {
+		checkNode.Left = nil
 	}
 	if parent != nil {
-		parent.Left = leftSon
+		if parent.Left == checkNode {
+			parent.Left = leftSon
+		} else {
+			parent.Right = leftSon
+		}
 	}
 
 	// udpate height
@@ -176,9 +193,15 @@ func (avl *AVL) RR(checkNode *AVL) *AVL {
 	if rightSonLeftSon != nil {
 		checkNode.Right = rightSonLeftSon
 		rightSonLeftSon.Parent = checkNode
+	} else {
+		checkNode.Right = nil
 	}
 	if parent != nil {
-		parent.Right = rightSon
+		if parent.Left == checkNode {
+			parent.Left = rightSon
+		} else {
+			parent.Right = rightSon
+		}
 	}
 
 	// udpate height
@@ -208,13 +231,21 @@ func (avl *AVL) LR(checkNode *AVL) *AVL {
 	if leftSonRightSonLeftSon != nil {
 		leftSonRightSonLeftSon.Parent = leftSon
 		leftSon.Right = leftSonRightSonLeftSon
+	} else {
+		leftSon.Right = nil
 	}
 	if leftSonRightSonRightSon != nil {
 		leftSonRightSonRightSon.Parent = checkNode
 		checkNode.Left = leftSonRightSonRightSon
+	} else {
+		checkNode.Left = nil
 	}
 	if parent != nil {
-		parent.Left = leftSonRightSon
+		if parent.Left == checkNode {
+			parent.Left = leftSonRightSon
+		} else {
+			parent.Right = leftSonRightSon
+		}
 	}
 
 	// update height
@@ -250,13 +281,21 @@ func (avl *AVL) RL(checkNode *AVL) *AVL {
 	if rightSonLeftSonLeftSon != nil {
 		rightSonLeftSonLeftSon.Parent = checkNode
 		checkNode.Right = rightSonLeftSonLeftSon
+	} else {
+		checkNode.Right = nil
 	}
 	if rightSonLeftSonRightSon != nil {
 		rightSonLeftSonRightSon.Parent = rightSon
 		rightSon.Left = rightSonLeftSonRightSon
+	} else {
+		rightSon.Left = nil
 	}
 	if parent != nil {
-		parent.Right = rightSonLeftSon
+		if parent.Left == checkNode {
+			parent.Left = rightSonLeftSon
+		} else {
+			parent.Right = rightSonLeftSon
+		}
 	}
 
 	// update height
@@ -313,9 +352,211 @@ func (avl *AVL) printSub() {
 	if avl.Left != nil {
 		avl.Left.printSub()
 	}
-	fmt.Print(avl.data, " ")
+	fmt.Print("[", avl.data, ",", avl.pos, "] ")
 	if avl.Right != nil {
 		avl.Right.printSub()
+	}
+}
+
+// consolePrint 打印二叉树
+// 可自行扩展打印格式
+func (avl *AVL) consolePrint() {
+	fmt.Println()
+	fmt.Println("============= AVL ===============")
+	s := make([]*AVL, 1)
+	s[0] = avl.root()
+
+	h := avl.root().depth() + 1
+	for i := 0; i < h; i++ {
+		len := len(s)
+		printed := 0.0
+		for _, node := range s {
+			printed = node.show(printed)
+		}
+		fmt.Println()
+
+		// 划线
+		// printed = 0.0
+		// for _, node := range s {
+		//     printed = node.showLine1(printed)
+		// }
+		// fmt.Println()
+
+		printed = 0.0
+		for _, node := range s {
+			printed = node.showLine2(printed)
+		}
+		fmt.Println()
+
+		for _, node := range s {
+			if node.Left != nil {
+				s = append(s, node.Left)
+			}
+			if node.Right != nil {
+				s = append(s, node.Right)
+			}
+		}
+		s = s[len:]
+	}
+
+	fmt.Println("=================================")
+	fmt.Println()
+}
+
+const halfItemSpace string = "   "
+const itemSpace string = "      "
+
+// show 显示一个节点
+// 一个节点的显示方式是 [abcd] 共6位, 可根据实际调整
+func (avl *AVL) show(printed float64) float64 {
+	// show space
+	for i := 0; i < int(avl.pos-printed); i++ {
+		fmt.Print(itemSpace)
+	}
+	if printed == 0 && avl.height != avl.root().depth() {
+		fmt.Print(halfItemSpace)
+	}
+
+	// show data, 后面可以优化为居中显示
+	{
+		switch len(strconv.Itoa(avl.data)) {
+		case 1:
+			fmt.Print("[ ", avl.data, "  ]")
+		case 2:
+			fmt.Print("[ ", avl.data, " ]")
+		case 3:
+			fmt.Print("[ ", avl.data, "]")
+		case 4:
+			fmt.Print("[", avl.data, "]")
+		default:
+			fmt.Print("[abcd]")
+		}
+	}
+
+	return avl.pos + 1
+}
+
+const line1 string = "     |"
+
+func (avl *AVL) showLine1(printed float64) float64 {
+	// |
+	if avl.Left == nil && avl.Right == nil {
+		return 0.0
+	}
+
+	for i := 0; i < int(avl.pos-printed); i++ {
+		fmt.Print(itemSpace)
+	}
+	fmt.Print(line1)
+
+	return float64(int(avl.pos) + 1)
+}
+
+const line2 string = "  -"
+const line3 string = "------"
+const line4 string = "---"
+
+func (avl *AVL) showLine2(printed float64) float64 {
+	// -
+	newPrinted := printed
+	if avl.height != avl.root().depth()-1 {
+		if avl.Left != nil {
+			for i := 0; i < int(avl.Left.pos+0.5-newPrinted); i++ {
+				fmt.Print(itemSpace)
+			}
+			if avl.height == avl.root().depth()-1 {
+				// if float64(int(avl.Left.pos-newPrinted)) != avl.Left.pos-newPrinted {
+				fmt.Print(halfItemSpace)
+				// }
+			}
+			for i := 0; i < int(avl.pos-avl.Left.pos); i++ {
+				fmt.Print(line3)
+			}
+			if avl.height == avl.root().depth()-1 {
+				fmt.Print(line4)
+			}
+			newPrinted = avl.pos + 0.5
+		}
+		if avl.Right != nil {
+			if avl.Left == nil {
+				for i := 0; i < int(avl.pos+0.5-newPrinted); i++ {
+					fmt.Print(itemSpace)
+				}
+				if float64(int(avl.pos+0.5-newPrinted)) != avl.pos+0.5-newPrinted {
+					fmt.Print(halfItemSpace)
+				}
+				newPrinted = avl.pos + 0.5
+			}
+			for i := 0; i < int(avl.Right.pos+0.5-newPrinted); i++ {
+				fmt.Print(line3)
+			}
+			if avl.height == avl.root().depth()-1 {
+				fmt.Print(line4)
+			}
+
+			newPrinted = avl.Right.pos + 0.5
+		}
+	} else {
+		// show space
+		for i := 0; i < int(avl.pos-newPrinted); i++ {
+			fmt.Print(itemSpace)
+		}
+		if printed == 0 && avl.height != avl.root().depth() {
+			fmt.Print(halfItemSpace)
+		}
+
+		if avl.Left != nil {
+			fmt.Print("---")
+		} else {
+			fmt.Print("   ")
+		}
+		if avl.Right != nil {
+			fmt.Print("---")
+		} else {
+			fmt.Print("   ")
+		}
+
+		newPrinted = avl.pos + 1
+
+	}
+
+	return newPrinted
+}
+
+func pow2(y int) (ret int) {
+	ret = 1
+	for i := 0; i < y; i++ {
+		ret *= 2
+	}
+
+	return
+}
+
+// calcPos 计算打印位置
+// 层次遍历
+func (avl *AVL) calcPos() {
+	interval := pow2(avl.root().depth()-avl.height) / 2
+	if avl.Parent == nil {
+		avl.pos = float64(interval) - 0.5
+	} else {
+		if avl == avl.Parent.Left {
+			avl.pos = avl.Parent.pos - float64(interval)
+			if interval == 0 {
+				avl.pos -= 0.5
+			}
+		} else {
+			avl.pos = avl.Parent.pos + float64(interval)
+			if interval == 0 {
+				avl.pos += 0.5
+			}
+		}
+	}
+
+	if avl.Left != nil {
+		avl.Left.calcPos()
+	}
+	if avl.Right != nil {
+		avl.Right.calcPos()
 	}
 }
 
@@ -324,14 +565,19 @@ func main() {
 	node := AVL{data: 1}
 	root := &node
 
-	for i := 0; i < 10; i++ {
-		rand.Seed(time.Now().Unix())
-		newNode = node.add(rand.Intn(50))
+	for i := 0; i < 30; i++ {
+		rand.Seed(time.Now().UnixNano())
+		newNode = root.add(rand.Intn(50))
 		checkNode = newNode.check()
 		if checkNode != nil {
 			root = root.balance(newNode, checkNode)
 		}
-		root.printSub()
-		time.Sleep(time.Second)
+		fmt.Println()
+		fmt.Println("new item: ", newNode.data)
+		root.calcPos()
+		root.print()
+		time.Sleep(time.Millisecond * 10)
 	}
+
+	root.consolePrint()
 }
