e57d1752a5999e37eb3b34c68e59fd16fa14b2d0
63isOK <1876180681@qq.com>
2019-04-01 05:54:03 +0000

    go标准容器 heap list ring

--- mastering_go/src005/heap.go
+++ mastering_go/src005/heap.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"container/heap"
+	"fmt"
+)
+
 type heapFloat32 []float32
 
 func (n *heapFloat32) Pop() interface{} {
@@ -11,5 +16,32 @@ func (n *heapFloat32) Pop() interface{} {
 	return x
 }
 
+func (n *heapFloat32) Push(x interface{}) {
+	*n = append(*n, x.(float32))
+}
+
+func (n heapFloat32) Len() int {
+	return len(n)
+}
+
+func (n heapFloat32) Less(a, b int) bool {
+	return n[a] < n[b]
+}
+
+func (n heapFloat32) Swap(a, b int) {
+	n[a], n[b] = n[b], n[a]
+}
+
 func main() {
+	h := &heapFloat32{6.0, 2.0, 3.0, 4.0, 5.0}
+	fmt.Println("%v\n", h)
+	heap.Init(h)
+	fmt.Println("%v\n", h)
+
+	h.Push(float32(12.0))
+	h.Push(float32(-12.0))
+	fmt.Println("%v\n", h)
+
+	heap.Init(h)
+	fmt.Println("%v\n", h)
 }
