074630191e562d3b9223859750f468b905a81b71
63isOK <1876180681@qq.com>
2019-07-25 07:29:47 +0000

    补充encoding/json的知识

--- kurento-client-go/test/ws/server/hub.go
+++ kurento-client-go/test/ws/server/hub.go
@@ -1,6 +1,8 @@
 package main
 
 // Hub 维护client,并负责向client发送消息
+// Hub 维护了所有的client和3个通道(负责注册/注销/广播消息)
+// Hub 完成了3个功能:负责client的注册和销毁;接到消息后广播给所有client
 type Hub struct {
 	clients    map[*Client]bool
 	broadcast  chan []byte
@@ -30,8 +32,8 @@ func (h *Hub) run() {
 		case message := <-h.broadcast:
 			for client := range h.clients {
 				select {
-				case cleint.send <- message:
-				defautl:
+				case client.send <- message:
+				default:
 					close(client.send)
 					delete(h.clients, client)
 				}
